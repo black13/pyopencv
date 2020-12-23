4ffc776b11673dbe5379da832e73ce5fb113ddbd
black_13 <jjosburn@gmail.com>
2020-12-22 19:32:21 -0600

    this will split images

--- lines.py
+++ lines.py
@@ -5,7 +5,7 @@ import random as rng
 
 #https://stackoverflow.com/questions/48615935/merging-regions-in-mser-for-identifying-text-lines-in-ocr
 
-image = cv2.imread('/home/jjosburn/temp/pytorch_models/images/outputname-0017.png',cv2.IMREAD_GRAYSCALE)
+image = cv2.imread('/home/jjosburn/temp/pytorch_models/images/outputname-0018.png',cv2.IMREAD_GRAYSCALE)
 (thresh, img_bin) = cv2.threshold(image, 128, 255,cv2.THRESH_BINARY|cv2.THRESH_OTSU)
 img_bin = 255-img_bin 
 kernel_length = np.array(image).shape[1]//80
@@ -28,8 +28,22 @@ verticle_lines_img = cv2.dilate(img_temp1, verticle_kernel, iterations=3)
 img_temp2 = cv2.erode(img_bin, hori_kernel, iterations=3)
 horizontal_lines_img = cv2.dilate(img_temp2, hori_kernel, iterations=3)
 
-# now show large contour
+cnts = cv2.findContours(horizontal_lines_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+for c in cnts:
+    cv2.drawContours(img_bin, [c], -1, (0,0,0), 3)
+
 drawing = np.zeros((image.shape[0], image.shape[1], 3), dtype=np.uint8)
+for i in range(len(cnts)):
+    color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
+    hull = cv2.convexHull(cnts[i]) 
+    cv2.drawContours(drawing, cnts, i, color)
+    color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
+    cv2.drawContours(drawing, hull, i, color)
+    boundRect = cv2.boundingRect(hull)
+
+# now show large contour
+
 cnts = cv2.findContours(verticle_lines_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
